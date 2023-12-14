b61c8c740c29e53567f42cb1092cf391550df166
Cloud <ruben.as532@gmail.com>
2023-12-14 10:25:41 +0100

    What I did in 30 minutes(UNCOMPLETE)

--- index.php
+++ index.php
@@ -1,13 +1,16 @@
 <?php
 /*
-TODO: -use an enum and not a boolean?
+TODO: 
+ - bug with rest of same element arrays.
+ -use an enum and not a boolean?
  - Enter numbers by terminal.
  - Do it without in array?
  - optimize array search having a reference array?
+ - This should be in a class :)
 */
 
 
-function operateArrays(array $array1, array $array2, boolean $option): array {
+function operateArrays(array $array1, array $array2, bool $option): array {
 	if($option) return intersectArrays($array1,$array2);
 	else return restArrays($array1, $array2); 
 }
@@ -20,9 +23,21 @@ function intersectArrays(array $array1, array $array2): array {
 	return $intersection_array;
 }
 
-$array1 = [7,2,9,12,5];
-$array2 = [4,2,6,1,2];
+function restArrays(array $array1, array $array2): array {
+	$intersection_array = intersectArrays($array1,$array2);
+	foreach($intersection_array as $element) {
+		$search_array1 = array_search($element, $array1);
+		$search_array2 = array_search($element, $array2);
+		if($search_array1) array_splice($array1,$search_array1);
+		if($search_array2) array_splice($array2,$search_array2);
+	}
+	return array_merge($array1,$array2);
+}
 
-var_dump(intersectArrays($array1, $array2));
+$array1 = [7,2,9,12,5];
+$array2 = [7,2,9,12,5];
+
+//var_dump(intersectArrays($array1, $array2));
+var_dump(restArrays($array2, $array1));
 
 ?>
